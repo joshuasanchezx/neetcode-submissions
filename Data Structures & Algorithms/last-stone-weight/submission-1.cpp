4d7d8ccc78a3e4329ed1e17a8c736cc673348a85
Joshua Sanchez <joshuasanchez_3@icloud.com>
2026-04-15 22:17:39 -0500

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        std::priority_queue<int> pq{};
+
+        for (const auto s : stones) pq.push(s); //store every stone in pq
+        while (pq.size() > 1){ //ensures theres always two stones
+            int x = pq.top(); pq.pop(); //gets first heaviest stone
+            int y = pq.top(); pq.pop(); //gets second heaviest stone
+
+            if (x != y){ pq.push(x - y);}
+        }
+        return pq.empty() ? 0 : pq.top();
+    }
+};
